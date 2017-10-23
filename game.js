0e76204d140c855c31bccd8eeac87b2c5e9e3ee6
Nikhil <nikhil.celtics@gmail.com>
2017-10-23 23:58:09 +0530

    removed commented code

--- game.js
+++ game.js
@@ -79,21 +79,7 @@ function startGame(){
 	obstacle.checkCollision(hero);
 	hero.accelerate(true, true);
 	obstacle.accelerate(true, true);
-/*	
-	var elements = {};
 
-	var hero = new gameElements(canvas.width, canvas.height, 0.1*canvas.width, 0.55*canvas.height, true);
-	hero.setImage(ctx, "images/hero.png");
-	hero.setSpeed(0.05*canvas.width, 0.005*canvas.height);
-	var ground = new gameElements(canvas.width, 1, 0, 0.7*canvas.height, false);
-	ground.draw(ctx, false);
-	var obstacle = new gameElements(canvas.width, canvas.height, 0.5*canvas.width, 0.6*canvas.height, true);
-	obstacle.setImage(ctx, "images/obstacle.png");
-
-	elements["hero"] = hero;
-	elements["ground"] = ground;
-	elements["obstacle"] = obstacle;
-*/
 	document.addEventListener('keydown', function(event){
 		eventHandler(event, ctx, hero);
 	});
@@ -104,68 +90,6 @@ function eventHandler(event, ctx, hero){
 
 	if (event.keyCode == 38 && hero.ongoingJump == 0){
 		hero.jump(ctx, hero.posy, 0);
-		/*var initialHeight = elements["hero"].posy;
-		var jumpState = setInterval(function(){executeJump(jumpState, ctx, elements, initialHeight);}, 1);
-	*/}
-
-}
-
-/*function executeJump(state, ctx, elements, initialHeight){
-
-	if (initialHeight - elements["hero"].posy > 0.5*initialHeight){
-		switchState(state, ctx, elements, initialHeight);
-	}
-	else{
-		jump(ctx, elements, "hero");
 	}
 
 }
-
-function switchState(state, ctx, elements, initialHeight){
-
-	clearInterval(state);
-	var descentState = setInterval(function(){executeDescent(descentState, ctx, elements, initialHeight);}, 1);
-
-}
-
-function executeDescent(state, ctx, elements, initialHeight){
-
-	if (initialHeight <= elements["hero"].posy){
-		endState(state);
-	}
-	else{
-		descend(ctx, elements, "hero");
-	}
-
-}
-
-function endState(state){
-
-	clearInterval(state);
-	ongoingEvent = 0;
-
-}
-
-function jump(ctx, elements, character){
-
-	ctx.clearRect(0, 0, canvas.width, canvas.height);
-	elements["ground"].draw(ctx, false);
-	elements["obstacle"].draw(ctx, true);
-	elements[character].posy -= elements[character].speedy;
-	if (elements[character].isImage){
-		elements[character].draw(ctx, true);
-	}
-
-}
-
-function descend(ctx, elements, character){
-
-	ctx.clearRect(0, 0, canvas.width, canvas.height);
-	elements["ground"].draw(ctx, false);
-	elements[character].posy += elements[character].speedy;
-	if (elements[character].isImage){
-		elements[character].draw(ctx, true);
-	}
-
-}
-*/
